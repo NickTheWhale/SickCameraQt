981fb96f61bdd98b40db296f996ec02f3f363d3a
NickTheWhale <loehrketurkey7@gmail.com>
2023-06-15 22:11:41 -0500

    working on test parser

--- TestParser/main.cpp
+++ TestParser/main.cpp
@@ -4,6 +4,11 @@
 #include <format>
 
 void parseBuffer(std::vector<uint8_t> buffer);
+std::string readFlexString(const std::vector<uint8_t> buffer, size_t& pos);
+std::string readNetAddress(const std::vector<uint8_t> buffer, size_t& pos);
+std::string readMacAddress(const std::vector<uint8_t> buffer, size_t& pos);
+std::string readFixString4(const std::vector<uint8_t> buffer, size_t& pos);
+uint32_t readUDint(const std::vector<uint8_t> buffer, size_t& pos);
 
 const std::vector<uint8_t> testBuffer = {
 	0x00,0x24,0x9b,0x13,0x73,0x3f,0x00,0x06,0x77,0x16,0x0e,0x55,0x08,0x00,0x45,0x00,0x01,0x24,0xbd,0x22,0x40,0x00,0x40,0x11,0x2c,0xa4,0xa9,0xfe,0x31,0xa1,0xa9,0xfe,0xca,0x64,0x77,0xfe,0x77,0xff,0x01,0x10,0xa5,0xfd,0x95,0x00,0x00,0xf8,0x00,0x06,0x77,0x16,0x0e,0x55,0x83,0x37,0x50,0x69,0x10,0x00,0x01,0x00,0x00,0x1d,0x56,0x69,0x73,0x69,0x6f,0x6e,0x61,0x72,0x79,0x2d,0x54,0x20,0x4d,0x69,0x6e,0x69,0x20,0x43,0x58,0x20,0x56,0x33,0x53,0x31,0x30,0x35,0x2d,0x31,0x78,0x00,0x01,0x00,0x06,0x00,0x00,0x00,0x00,0x74,0xc3,0x03,0x00,0x00,0x00,0x00,0x0c,0x56,0x69,0x73,0x69,0x6f,0x6e,0x61,0x72,0x79,0x20,0x41,0x50,0x00,0x0b,0x6e,0x6f,0x74,0x20,0x64,0x65,0x66,0x69,0x6e,0x65,0x64,0x00,0x0c,0x56,0x69,0x73,0x69,0x6f,0x6e,0x61,0x72,0x79,0x20,0x41,0x50,0x00,0x08,0x32,0x31,0x34,0x36,0x30,0x30,0x37,0x35,0x00,0x07,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x00,0x0b,0x31,0x2e,0x33,0x2e,0x30,0x2e,0x32,0x39,0x38,0x39,0x31,0x00,0x07,0x31,0x31,0x31,0x32,0x36,0x34,0x39,0x1e,0x00,0x02,0x53,0x65,0x67,0x53,0x00,0x04,0x00,0x00,0x40,0x00,0x41,0x75,0x74,0x56,0x00,0x08,0x30,0x2e,0x31,0x2e,0x30,0x2e,0x30,0x52,0x00,0x01,0x02,0x02,0x00,0x08,0x4c,0x4f,0x4f,0x50,0x42,0x41,0x43,0x4b,0x00,0x05,0x45,0x49,0x50,0x61,0x00,0x04,0xa9,0xfe,0x31,0xa1,0x45,0x4e,0x4d,0x61,0x00,0x04,0xff,0xff,0x00,0x00,0x45,0x44,0x47,0x61,0x00,0x04,0x00,0x00,0x00,0x00,0x45,0x43,0x44,0x75,0x00,0x04,0x00,0x00,0x00,0x05,0x45,0x4d,0x41,0x43,0x00,0x06,0x00,0x06,0x77,0x16,0x0e,0x55,0x00,0x02,0x01,0x00,0x01,0x44,0x50,0x4e,0x6f,0x00,0x02,0x08,0x4a,0x03,0x00,0x01,0x44,0x50,0x4e,0x6f,0x00,0x02,0x08,0x3f
@@ -11,13 +16,69 @@ const std::vector<uint8_t> testBuffer = {
 
 int main()
 {
-
 	parseBuffer(testBuffer);
-
-
 
 	std::cin.get();
 	return 0;
+}
+
+std::string readFlexString(const std::vector<uint8_t> buffer, size_t& pos)
+{
+	std::string flexString = "";
+	uint16_t size = buffer[pos++] << 8 | buffer[pos++];
+	for (size_t i = 0; i < size; ++i)
+	{
+		flexString.push_back(static_cast<uint8_t>(buffer[pos++]));
+	}
+	return flexString;
+}
+
+std::string readNetAddress(const std::vector<uint8_t> buffer, size_t& pos)
+{
+	std::string address = "";
+	// we assume the length is 4
+	address.append(std::to_string(buffer[pos++]));
+	address.append(":");
+	address.append(std::to_string(buffer[pos++]));
+	address.append(":");
+	address.append(std::to_string(buffer[pos++]));
+	address.append(":");
+	address.append(std::to_string(buffer[pos++]));
+	return address;
+}
+
+std::string readMacAddress(const std::vector<uint8_t> buffer, size_t& pos)
+{
+	std::string macAddress = "";
+	// we assume the length is 6
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	macAddress.append(":");
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	macAddress.append(":");
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	macAddress.append(":");
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	macAddress.append(":");
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	macAddress.append(":");
+	macAddress.append(std::format("{:02X}", buffer[pos++]));
+	return macAddress;
+}
+
+std::string readFixString4(const std::vector<uint8_t> buffer, size_t& pos)
+{
+	std::string id = "";
+	const auto size = 4;
+	for (size_t i = 0; i < size; ++i)
+	{
+		id.push_back(static_cast<uint8_t>(buffer[pos++]));
+	}
+	return id;
+}
+
+uint32_t readUDint(std::vector<uint8_t> buffer, size_t& pos)
+{
+	return buffer[pos++] | buffer[pos++] << 8 | buffer[pos++] << 16 | buffer[pos++] << 24;
 }
 
 void parseBuffer(std::vector<uint8_t> buffer)
@@ -71,33 +132,22 @@ void parseBuffer(std::vector<uint8_t> buffer)
 		// ignore isBC
 		pos += 1;
 
-
 		// get data size
 		auto msb = static_cast<size_t>(buffer[pos++]);
 		auto lsb = static_cast<uint8_t>(buffer[pos++]);
 		header.dataSize = (msb << 8) | lsb;
 
 		// get mac address
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
-		header.macAddress.append(":");
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
-		header.macAddress.append(":");
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
-		header.macAddress.append(":");
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
-		header.macAddress.append(":");
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
-		header.macAddress.append(":");
-		header.macAddress.append(std::format("{:02X}", buffer[pos++]));
+		header.macAddress = readMacAddress(buffer, pos);
 
 		// get telegram request id
-		header.id = buffer[pos++] | buffer[pos++] << 8 | buffer[pos++] << 16 | buffer[pos++] << 24;
+		header.id = readUDint(buffer, pos);
 
 		// idk
 		header.cs = buffer[pos++];
 
 		// ignore ffu
-		++pos;
+		pos += 1;
 
 		break;
 	}
@@ -112,52 +162,122 @@ void parseBuffer(std::vector<uint8_t> buffer)
 
 	while (true)
 	{
-		// ignore device info version
+		// ignore DeviceInfoVersion
 		pos += 2;
 
-		// get name
-		uint16_t nameSize = buffer[pos++] << 8 | buffer[pos++];
-		for (int i = 0; i < nameSize; ++i)
+		// get CidName
+		dev.DeviceName = readFlexString(buffer, pos);
+
+		// get CidVersion
+		struct CidVersion
 		{
-			dev.DeviceName.push_back(static_cast<char>(buffer[pos++]));
+			uint16_t major;
+			uint16_t minor;
+			uint16_t patch;
+			uint32_t buildNumber;
+			uint8_t classifier;
+		};
+		CidVersion ver;
+		ver.major = buffer[pos++] << 8 | buffer[pos++];
+		ver.minor = buffer[pos++] << 8 | buffer[pos++];
+		ver.patch = buffer[pos++] << 8 | buffer[pos++];
+		ver.buildNumber = buffer[pos++] << 24 | buffer[pos++] << 16 | buffer[pos++] << 8 | buffer[pos++];
+		ver.classifier = buffer[pos++];
+
+		// ignore DeviceState
+		pos += 1;
+
+		// ignore RequiredUserAction
+		pos += 2;
+
+		// get DeviceName
+		std::string deviceName = readFlexString(buffer, pos);
+
+		// get ApplicationSpecificName
+		std::string applicationSpecificName = readFlexString(buffer, pos);
+
+		// ignore ProjectName
+		std::string projectName = readFlexString(buffer, pos);
+
+		// ignore SerialNumber
+		std::string serialNumberSize = readFlexString(buffer, pos);
+
+		// ignore TypeCode
+		std::string typeCode = readFlexString(buffer, pos);
+
+		// ignore FirmwareVersion
+		std::string firmwareVersion = readFlexString(buffer, pos);
+
+		// ignore OrderNumber
+		std::string orderNumber = readFlexString(buffer, pos);
+
+		// ignore Flags
+		pos += 1;
+
+		// get Aux Entries
+		struct AuxEntry
+		{
+			std::string id;
+			std::string value;
+		};
+
+		std::vector<AuxEntry> entries;
+
+		uint16_t auxEntrySize = buffer[pos++] << 8 | buffer[pos++];
+		for (size_t i = 0; i < auxEntrySize; ++i)
+		{
+			AuxEntry entry;
+			entry.id = readFixString4(buffer, pos);
+			entry.value = readFlexString(buffer, pos);
+			entries.push_back(entry);
 		}
 
-		// ignore version 
-		pos += 11;
+		// get Scan IF
+		struct ScanIF
+		{
+			uint16_t number;
+			std::string value;
+		};
 
-		// ignore device state
-		pos += 1;
+		std::vector<ScanIF> scanInterfaces;
 
-		// ignore required user action
-		pos += 2;
+		uint16_t scanIFSize = buffer[pos++] << 8 | buffer[pos++];
+		for (size_t i = 0; i < scanIFSize; ++i)
+		{
+			ScanIF scan;
+			scan.number = buffer[pos++] << 8 | buffer[pos++];
+			scan.value = readFlexString(buffer, pos);
+			scanInterfaces.push_back(scan);
+		}
 
-		// ignore device name
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore app spec name
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore project name
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore serial number
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore type code
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore firmware version
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore order number
-		pos += buffer[pos++] << 8 | buffer[pos++];
-
-		// ignore flags
-		pos += 1;
-
-		// parse aux entries for ip address, port number, and subnet
-
-
+		//General Com Settings
+		uint16_t comSettingSize = buffer[pos++] << 8 | buffer[pos++];
+		for (size_t i = 0; i < comSettingSize; ++i)
+		{
+			std::string key = readFixString4(buffer, pos);
+			// IpAddress
+			if (key == "EIPa")
+			{
+				pos += 2;
+				dev.IpAddress = readNetAddress(buffer, pos);
+			}
+			// SubnetMask
+			else if (key == "ENMa")
+			{
+				pos += 2;
+				dev.SubNet = readNetAddress(buffer, pos);
+			}
+			//MacAddress
+			else if (key == "EMAC")
+			{
+				dev.MacAddress = readMacAddress(buffer, pos);
+			}
+			// advance the buffer if we don't care about the value
+			else
+			{
+				readFlexString(buffer, pos);
+			}
+		}
 
 		break;
 	}
